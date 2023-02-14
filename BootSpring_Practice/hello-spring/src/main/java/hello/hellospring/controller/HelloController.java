88d1c73121106ed4bde5bf3da1f3980187401e5a
csh7099 <ccsh7099@gmail.com>
2023-02-14 20:56:01 +0900

    springstudy(230214)

--- BootSpring_Practice/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ BootSpring_Practice/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,2 +1,15 @@
-package hello.hellospring.controller;public class HelloController {
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){   //Model은 MVC의 Model느낌
+//        value에는 데이터를 넣어주면 data변수로 사용 가능함
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
 }
